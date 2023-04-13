e24e1e62dac0b5231f941a0f8c70f09f9ed3b1df
MomoAgui <git@github.com:MomoAgui/WEBCAMP_PHP_Beginner.git>
2023-04-13 12:40:34 +0000

    非ログイントップページ作成

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
-
-/*
-Route::get('/',function(){
-    return view('welcome');
-});
-*/
+use App\Http\Controllers\AuthController;
 
 
+//テスト用
 Route::get('/welcome',[WelcomeController::class,'index']);
 Route::get('/welcome/second',[WelcomeController::class,'second']);
+
+//タスク管理システム
+Route::get('/',[AuthController::class,'index']);
