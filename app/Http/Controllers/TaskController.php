aaef18ab188a79f9ad7ed5c3e13073892ebecf80
MomoAgui <git@github.com:MomoAgui/WEBCAMP_PHP_Beginner.git>
2023-04-21 13:10:58 +0000

    タスクの一覧出力を書いた

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,23 +1,41 @@
 <?php
 declare(strict_types=1);
 namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
 use App\Http\Requests\TaskRegisterPostRequest;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Task as TaskModel;
-use App\Http\Controllers\Controller;
 
 class TaskController extends Controller
 {
-       /**
+    /**
      * タスク一覧ページ を表示する
      *
      * @return \Illuminate\View\View
      */
     public function list()
     {
-        return view('task.list');
-}
-/**
+        // 一覧の取得
+        $list = TaskModel::where('user_id', Auth::id())
+                         ->orderBy('priority', 'DESC')
+                         ->orderBy('period')
+                         ->orderBy('created_at')
+                         ->get();
+/*
+$sql = TaskModel::where('user_id', Auth::id())
+                 ->orderBy('priority', 'DESC')
+                 ->orderBy('period')
+                 ->orderBy('created_at')
+                 ->toSql();
+//echo "<pre>\n"; var_dump($sql, $list); exit;
+var_dump($sql);
+*/
+        //
+        return view('task.list', ['list' => $list]);
+    }
+
+    /**
      * タスクの新規登録
      */
     public function register(TaskRegisterPostRequest $request)
@@ -35,17 +53,16 @@ class TaskController extends Controller
         // テーブルへのINSERT
         try {
             $r = TaskModel::create($datum);
-var_dump($r); exit;
         } catch(\Throwable $e) {
             // XXX 本当はログに書く等の処理をする。今回は一端「出力する」だけ
             echo $e->getMessage();
             exit;
         }
+
         // タスク登録成功
         $request->session()->flash('front.task_register_success', true);
 
         //
         return redirect('/task/list');
     }
-
-    }
+}
