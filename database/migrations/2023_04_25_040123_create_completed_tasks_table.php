401d16bb613334cc587e42986924287f398d4e72
MomoAgui <git@github.com:MomoAgui/WEBCAMP_PHP_Beginner.git>
2023-04-25 05:49:33 +0000

    タスクの削除と完了を書いた

--- database/migrations/2023_04_25_040123_create_completed_tasks_table.php
+++ database/migrations/2023_04_25_040123_create_completed_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTasksTable extends Migration
+class CreateCompletedTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class CreateTasksTable extends Migration
      */
     public function up()
     {
-        Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+        Schema::create('completed_tasks', function (Blueprint $table) {
+            $table->unsignedInteger('id');
             $table->string('name', 128)->comment('タスク名');
             $table->date('period')->comment('タスクの期限');
             $table->text('detail')->comment('タスクの詳細');
@@ -22,8 +22,10 @@ class CreateTasksTable extends Migration
             $table->unsignedBigInteger('user_id')->comment('このタスクの所有者');
             $table->foreign('user_id')->references('id')->on('users'); // 外部キー制約
             //$table->timestamps();
-            $table->dateTime('created_at')->useCurrent()->comment('タスク完了日');
+            $table->dateTime('created_at')->useCurrent()->comment('タスク完了日時');
             $table->dateTime('updated_at')->useCurrent()->useCurrentOnUpdate();
+            //
+            $table->primary('id');
         });
     }
 
@@ -34,6 +36,6 @@ class CreateTasksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('completed_tasks');
     }
 }
